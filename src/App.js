2833f878e449e6a16f850ec804a497897c65c098
mcrae32 <mcrae32@mail.ru>
2022-12-07 17:33:01 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Greetings from './Greetings/Greetings.js';
+
+function App() {
+  return (
+    
+      <div>
+        <Greetings firstName="John" lastName="Smith" />
+      </div>
+    
+  );
+}
+
+export default App;
---
+++ src/Greetings/Greetings.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Greetings = (props) => (
+	<div>Hey you! {props.firstName} {props.lastName}!</div>
+)
+
+export default Greetings;
